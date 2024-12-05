37305f85c109522d9d42330ad58d5b10f8d397cc
carloshernandez02 <cahernandezn@unah.hn>
2024-12-05 02:28:46 -0600

    listtile enviado a otro archivo

--- cube_timer/lib/src/fragments/times_fragment.dart
+++ cube_timer/lib/src/fragments/times_fragment.dart
@@ -1,3 +1,4 @@
+import 'package:cube_timer/src/pages/Widgets/timer_item.dart';
 import 'package:flutter/material.dart';
 import 'package:hive_ce_flutter/hive_flutter.dart';
 import 'package:cube_timer/data/classes/box_names.dart';
@@ -14,34 +15,9 @@ class Times extends StatelessWidget {
       itemCount: solves.length,
       itemBuilder: (BuildContext context, int index) {
         final solve = solves.getAt(solves.length - index - 1);
-        return ListTile(
-          leading: Text(formatDate(solve?.date)),
-          title: Text(formatTime(solve?.time ?? Duration.zero))
-        );
+        return TimerItem(solve: solve);
       },
     );
   }
 }
 
-String formatTime(Duration time) {
-  if (time < Duration(minutes: 1)) {
-
-    return '${time.inSeconds}.${time.inMilliseconds % 1000}';
-  } else {
-
-    final minutes = time.inMinutes;
-    final seconds = time.inSeconds % 60; 
-    final milliseconds = time.inMilliseconds % 1000;
-    return '$minutes:${seconds.toString().padLeft(2, '0')}.$milliseconds';
-  }
-}
-
-String formatDate(DateTime? date) {
-  if (date == null) return '';
-  final hour = date.hour;
-  final minute = date.minute.toString().padLeft(2, '0');
-  final day = date.day;
-  final month = date.month;
-  final year = date.year;
-  return '$day/$month/$year at $hour:$minute';
-}
